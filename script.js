1f02b71e663cf658314881a6cd85f94b963c0fba
Nate <for.education.n@yandex.ru>
2021-05-07 22:51:39 +0200

    Added draft calculator workflow (with one digit)

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+const displayText = document.getElementById("display-text");
+const digitBtns = document.querySelectorAll(".digit");
+const operatorBtns = document.querySelectorAll(".operator");
+const clearBtn = document.querySelector(".clear");
+const equalsBtn = document.querySelector(".equals");
+
+let digit = "";
+let operator = "";
+
+digitBtns.forEach((digitBtn) => {
+  digitBtn.addEventListener("click", () => {
+    digit += digitBtn.textContent;
+    displayText.textContent = digit;
+  });
+});
+
+operatorBtns.forEach((operatorBtn) => {
+  operatorBtn.addEventListener("click", () => {
+    operator = operatorBtn.textContent;
+    displayText.textContent += ` ${operator}`;
+  });
+});
+
+clearBtn.addEventListener("click", () => {
+  displayText.textContent = "";
+  digit = "";
+  operator = "";
+});
+
+equalsBtn.addEventListener("click", () => {
+  if (digit !== "" && operator !== "") {
+    operate(operator, Number(digit), Number(digit));
+  }
+});
+
 function add(a, b) {
   return a + b;
 }
@@ -15,20 +50,22 @@ function divide(a, b) {
 }
 
 function operate(operator, numA, numB) {
+  let result = 0;
   switch (operator) {
     case "+":
-      add(numA, numB);
+      result = add(numA, numB);
       break;
     case "-":
-      substract(numA, numB);
+      result = substract(numA, numB);
       break;
     case "*":
-      multiply(numA, numB);
+      result = multiply(numA, numB);
       break;
     case ":":
-      divide(numA, numB);
+      result = divide(numA, numB);
       break;
     default:
       break;
   }
+  displayText.textContent = result;
 }
