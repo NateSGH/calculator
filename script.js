1798e73b55f3cbadf4e1d8b8e8dcbc6f842ff3fa
Nate <for.education.n@yandex.ru>
2021-05-11 01:15:04 +0200

    Added support of floating point numbers, keyboard support - first ver

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const displayText = document.getElementById("display-text");
 const digitBtns = document.querySelectorAll(".digit");
 const operatorBtns = document.querySelectorAll(".operator");
 const clearBtn = document.querySelector(".clear");
+const bkspBtn = document.querySelector(".backspace");
 const equalsBtn = document.querySelector(".equals");
+const pointBtn = document.querySelector(".point");
 
 let inputNum = "";
 let operator = "";
@@ -13,38 +15,24 @@ function resetAllVals() {
   operator = "";
   number = 0;
 }
+// TODO: finish
+// bkspBtn.addEventListener("click", () => {
+//   displayText.textContent = displayText.textContent.slice(0, -1);
+// });
+
+pointBtn.addEventListener("click", () => {
+  pointInput();
+});
 
 digitBtns.forEach((digitBtn) => {
   digitBtn.addEventListener("click", () => {
-    inputNum += digitBtn.textContent;
-    if (operator !== "") {
-      displayText.textContent = `${number} ${operator} ${inputNum}`;
-    } else displayText.textContent = inputNum;
+    numInput(digitBtn.textContent);
   });
 });
 
 operatorBtns.forEach((operatorBtn) => {
   operatorBtn.addEventListener("click", () => {
-    // let currentOperator = operator;
-    if (!number) {
-      // start
-      number = Number(inputNum);
-      inputNum = "";
-      operator = operatorBtn.textContent;
-      displayText.textContent = `${number} ${operator}`;
-    } else if (inputNum !== "") {
-      // second and other operators
-      let secondNum = Number(inputNum);
-      secondNum = operate(operator, number, secondNum);
-      resetAllVals();
-      operator = operatorBtn.textContent;
-      number = secondNum;
-      displayText.textContent = `${number} ${operator}`;
-    } else {
-      // after "equals" pressed
-      operator = operatorBtn.textContent;
-      displayText.textContent = `${number} ${operator}`;
-    }
+    operatorInput(operatorBtn.textContent);
   });
 });
 
@@ -54,13 +42,61 @@ clearBtn.addEventListener("click", () => {
 });
 
 equalsBtn.addEventListener("click", () => {
+  equals();
+});
+
+function pointInput() {
+  if (inputNum !== "" && !inputNum.includes(".")) {
+    inputNum += ".";
+
+    if (operator !== "") {
+      displayText.textContent = `${number} ${operator} ${inputNum}`;
+    } else displayText.textContent = inputNum;
+  }
+}
+
+function equals() {
+  console.log(`${number} ${operator} ${inputNum}`);
   if (inputNum !== "" && operator !== "" && inputNum !== 0) {
     let secondNum = Number(inputNum);
     secondNum = operate(operator, number, secondNum);
     resetAllVals();
     number = secondNum;
+    return number;
   }
-});
+}
+
+function numInput(digit) {
+  inputNum += digit;
+  if (inputNum.includes(".")) {
+    inputNum = inputNum.slice(0, inputNum.indexOf(".") + 2);
+  }
+  if (operator !== "") {
+    displayText.textContent = `${number} ${operator} ${inputNum}`;
+  } else displayText.textContent = inputNum;
+}
+
+function operatorInput(operatorVal) {
+  if (!number) {
+    // start
+    number = Number(inputNum);
+    inputNum = "";
+    operator = operatorVal;
+    displayText.textContent = `${number} ${operator}`;
+  } else if (inputNum !== "") {
+    // second and other operators
+    let secondNum = Number(inputNum);
+    secondNum = operate(operator, number, secondNum);
+    resetAllVals();
+    operator = operatorVal;
+    number = secondNum;
+    displayText.textContent = `${number} ${operator}`;
+  } else {
+    // after "equals" pressed
+    operator = operatorVal;
+    displayText.textContent = `${number} ${operator}`;
+  }
+}
 
 function add(a, b) {
   return a + b;
@@ -75,6 +111,9 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
+  if (b === 0) {
+    return "Error. Division by zero";
+  }
   return a / b;
 }
 
@@ -96,6 +135,95 @@ function operate(operator, numA, numB) {
     default:
       break;
   }
+  if (!Number.isInteger(result)) {
+    result = Number(result.toFixed(2));
+  }
+  console.log(`Operate Result - ${result} type ${typeof result}`);
   displayText.textContent = result;
+  console.log(`displayText - ${displayText.textContent}`);
+  // displayText.textContent += "Test";
   return result;
 }
+
+// Keyboard
+window.addEventListener("keydown", (e) => {
+  let operatorFlag = false;
+  let operatorTemp = "";
+
+  if (e.code.includes("Digit") && e.code.slice(-1) === e.key) {
+    numInput(e.key);
+  } else if (e.key === "Enter") {
+    console.log(`Enter pressed - ${equals()}`);
+    // equals();
+  } else if (
+    e.code.includes("Numpad") &&
+    !Number.isNaN(Number(e.code.slice(-1)))
+  ) {
+    numInput(e.key);
+    // let operatorFlag = false;
+    // let operatorTemp = "";
+
+    // switch (e.key) {
+    //   case "+":
+    //     operatorFlag = true;
+    //     operatorTemp = e.key;
+    //     break;
+    //   case "-":
+    //     operatorFlag = true;
+    //     operatorTemp = e.key;
+    //     break;
+    //   case "/":
+    //     operatorFlag = true;
+    //     operatorTemp = "÷";
+    //     break;
+    //   case "*":
+    //     operatorFlag = true;
+    //     operatorTemp = "×";
+    //     break;
+    //   case "Enter":
+    //     break;
+    //   default:
+    //     break;
+    // }
+
+    // if (operatorFlag) {
+    //   operatorInput(operatorTemp);
+    //   operatorFlag = false;
+    //   // } else if (e.code.includes("NumpadEnter")) {
+    //   // equals();
+    // } else if (e.key === ".") {
+    //   pointInput();
+    // } else {
+    //   numInput(e.key);
+    // }
+  } else if (e.key === ".") {
+    pointInput();
+  } else {
+    switch (e.key) {
+      case "+":
+        operatorFlag = true;
+        operatorTemp = e.key;
+        break;
+      case "-":
+        operatorFlag = true;
+        operatorTemp = e.key;
+        break;
+      case "/":
+        operatorFlag = true;
+        operatorTemp = "÷";
+        break;
+      case "*":
+        console.log(e);
+        operatorFlag = true;
+        operatorTemp = "×";
+        break;
+      default:
+        break;
+    }
+
+    if (operatorFlag) {
+      operatorInput(operatorTemp);
+      operatorFlag = false;
+    }
+  }
+});
